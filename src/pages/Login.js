cc211ba2adf9544a6369e69ef14027afe7917d6f
riteshreg <riteshregmi85@gmail.com>
2022-12-09 16:12:31 +0545

    login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,24 +9,42 @@ import { Input } from "@mui/material";
 import "./pages_style.css";
 
 export class Login extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      userId:"",
+      password:""
+    }
+  }
+
+   handleChange = (event) =>{
+    this.setState({[event.target.name]:event.target.value})
+  }
+
+  handleLogin = ()  =>{
+    console.log(this.state)
+  }
+
   render() {
 
     return (
       <div className="login-form_container">
         <div className="form_container">
         <FormControl className='login_form'>
-          <TextField id="outlined-basic" label="user id" variant="outlined" />
+          <TextField name="userId" value={this.state.userId} onChange={this.handleChange} id="outlined-basic" label="user id" variant="outlined" />
           <FormHelperText id="my-helper-text">
             Enter your admin userid
           </FormHelperText>
         </FormControl>
         <FormControl className="login_form">
-          <TextField style={{marginTop:'20px'}} id="outlined-basic" label="password" variant="outlined" />
+          <TextField name="password" value={this.state.password} onChange={this.handleChange} style={{marginTop:'20px'}} id="outlined-basic" label="password" variant="outlined" />
           <FormHelperText id="my-helper-text">
             enter your password
           </FormHelperText>
         </FormControl>
-        <Button style={{margin:"30px"}} variant="contained">Login</Button>
+        <Button onClick={this.handleLogin} style={{margin:"30px"}} variant="contained">Login</Button>
         </div>
       </div>
     );
