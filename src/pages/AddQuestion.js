dabf0652d08ad875811b9cb8c643ea095e5bdc54
riteshreg <riteshregmi85@gmail.com>
2022-12-10 09:16:43 +0545

    sd

--- src/pages/AddQuestion.js
+++ src/pages/AddQuestion.js
@@ -1,44 +1,83 @@
-import { Button, TextField } from '@mui/material'
-import React, { Component } from 'react'
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 
-
-export class AddQuestion extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = {
-      question:"",
-      answer:"",
-      option1:"",
-      opiton2:"",
-      option3:""
-    }
-  }
+const AddQuestion = () =>{
 
 
-  handleChange = (event) => {
+  const [formData, setFormData] = useState({
+    question:'',
+    answer:"",
+    option1:"",
+    option2:"",
+    option3:"" 
+  })
 
-  }
+  const handleChange = (event) => {
+    this.setState(
+        {  
+            ...formData,
+            [event.target.name] : event.target.value
+            
+        }
+      )
+  };
+
+  const handleAddQuestion = () => {
+    console.log(this.state);
+  };
 
 
-  render() {
     return (
       <div className="login-form_container">
-      <div className="form_container">
+        <div className="form_container">
+          <TextField
+            id="filled-basic"
+            name="question"
+            value={formData.question}
+            onChange={handleChange}
+            label="quesitons"
+            variant="filled"
+          />
+          <TextField
+            id="filled-basic"
+            name="answer"
+            value={formData.answer}
+            onChange={handleChange}
+            label="answer"
+            variant="filled"
+          />
+          <TextField
+            id="filled-basic"
+            name="option1"
+            value={formData.option1}
+            onChange={handleChange}
+            label="incorrect option"
+            variant="filled"
+          />
+          <TextField
+            id="filled-basic"
+            name="option2"
+            value={formData.option2}
+            label="incorrect option"
+            variant="filled"
+          />
+          <TextField
+            id="filled-basic"
+            name="option3"
+            value={formData.option3}
+            onChange={handleChange}
+            label="incorrect option"
+            variant="filled"
+          />
 
-      <TextField id="filled-basic"  name="question" value={this.state.question}  onChange={this.handleChange}  label="quesitons" variant="filled" />
-      <TextField id="filled-basic"  name="answer" value={this.state.answer} onChange={this.handleChange} label="answer" variant="filled" />
-      <TextField id="filled-basic"  name="option1" value={this.state.option1} onChange={this.handleChange}  label="incorrect option" variant="filled" />
-      <TextField id="filled-basic"  name="option2" value={this.state.option2} label="incorrect option" variant="filled" />
-      <TextField id="filled-basic" name="option3" value={this.state.option3} onChange={this.handleChange}  label="incorrect option" variant="filled" />
-
-      <Button variant='contained'>Add Question</Button>
-
+          <Button variant="contained" onClick={handleAddQuestion}>
+            Add Question
+          </Button>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
-}
 
-export default AddQuestion
+
+export default AddQuestion;
