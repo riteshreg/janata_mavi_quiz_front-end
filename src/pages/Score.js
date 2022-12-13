e35bd0e58fd58520138ed09af1b50377d8169950
riteshreg <riteshregmi85@gmail.com>
2022-12-13 10:07:04 +0545

    h

--- src/pages/Score.js
+++ src/pages/Score.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -14,11 +13,6 @@ export default function BasicCard({score}) {
 
   const Navigate = useNavigate();
 
-  React.useEffect(()=>{
-    if(localStorage.getItem("login")){
-      Navigate('/')
-    }
-  })
 
   return (
     <div className="scoreDiv">
