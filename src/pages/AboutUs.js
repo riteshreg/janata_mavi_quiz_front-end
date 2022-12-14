3a786ba4f8706bd7089625c2c3d394d18ae02f32
Ritesh Regmi <71575903+riteshreg@users.noreply.github.com>
2022-12-14 17:53:34 +0545

    update url

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -6,9 +6,9 @@ import { Typography } from "@mui/material";
 
 function AboutUs() {
 
-  const riteshImg =  "https://i.ibb.co/mDrr9Yj/IMG-3d7a803857c5c32485e9e613bac32da8-V.jpg"
-  const saugatImg = "https://iili.io/Hocrwfp.jpg"
-  const anupImg = "https://iili.io/HocI5dv.jpg"
+  const riteshImg =  "https://iili.io/Ho1iyzP.jpg"
+  const saugatImg = "https://iili.io/Ho1iHlf.jpg"
+  const anupImg = "https://iili.io/Ho1iMRp.jpg"
   const gopalImg = "https://scontent.fbdp1-1.fna.fbcdn.net/v/t1.15752-9/318416857_3344168082463081_5109954501792649591_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=ae9488&_nc_ohc=i0zlFS9x6SYAX_Makh0&_nc_ht=scontent.fbdp1-1.fna&oh=03_AdTn3y7giOvUmH_AGoj0he2y3wQVLm5OnXaFCddzt7qSNw&oe=63C1258C"
   const nabinaImg = "https://i.ibb.co/mDrr9Yj/IMG-3d7a803857c5c32485e9e613bac32da8-V.jpg"
 
