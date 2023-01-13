96313fcc03befb96bea0ea127f5bd8d2d70dd658
Ritesh Regmi <riteshregmi85@gmail.com>
2023-01-13 19:46:08 +0545

    pagination update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,84 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './component_style.css'
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import {useNavigate} from  'react-router-dom'    
+import React from "react";
+import { Link } from "react-router-dom";
+import "./component_style.css";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-  const  localSt = localStorage.getItem('login')
+  const localSt = localStorage.getItem("login");
   const Navigate = useNavigate();
 
-
-  const logout = () =>{
-
-    localStorage.clear()
-     Navigate("/login");
-
-  }
+  const logout = () => {
+    localStorage.clear();
+    Navigate("/login");
+  };
 
   return (
-    <div className='nav-bar_container' style={{minHeight:"3.6vh", fontSize:"0.98rem"}}>
-        <ul>
+    <div
+      className="nav-bar_container"
+      style={{ minHeight: "3.6vh", fontSize: "0.98rem" }}
+    >
+      <ul>
+        {localSt ? (
+          <>
+            <li>
+              <Link className="link" to="/admin">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/add-question">
+                Add Question
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/">
+                Play Quiz
+              </Link>
+            </li>
 
-        {
-          localSt? <>
-        <li><Link className='link' to='/admin'>Home</Link></li>
-        <li><Link className='link' to='/add-question'>Add Question</Link></li>
-        <li><Link className='link' to='/'>Play Quiz</Link></li>
-
-        <li><Link className="link" style={{position: "absolute", color:"white",
-    right: "0"}} onClick={logout} to='/login'><span className='admin_icon'>Logout({JSON.parse(localSt)})</span></Link></li>
+            <li>
+              <Link
+                className="link"
+                style={{ position: "absolute", color: "white", right: "0" }}
+                onClick={logout}
+                to="/login"
+              >
+                <span className="admin_icon">
+                  Logout({JSON.parse(localSt)})
+                </span>
+              </Link>
+            </li>
           </>
-      :
-      <>
-      <li><Link className='link' to='/'>Janata Quiz App</Link></li>
-      <div className="right_nav_item">
-
-      <li><Link className="link about_us" to='/about'>About Us</Link></li>
-      <li>
-        {
-             <Link className='link login' to='/login'> <span className='admin_icon'><AdminPanelSettingsIcon style={{color:"white"}}/></span></Link>
-
-        }     
-        </li>
-
-      </div>
-      
-        </>
-        }
-        </ul>
+        ) : (
+          <>
+            <li>
+              <Link className="link" to="/">
+                Janata Quiz App
+              </Link>
+            </li>
+            <div className="right_nav_item">
+              <li>
+                <Link className="link about_us" to="/about">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                {
+                  <Link className="link login" to="/login">
+                    {" "}
+                    <span className="admin_icon">
+                      <AdminPanelSettingsIcon style={{ color: "white" }} />
+                    </span>
+                  </Link>
+                }
+              </li>
+            </div>
+          </>
+        )}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -7,7 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { TablePagination } from "@mui/material";
+import { Pagination } from "@mui/material";
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
 import EditIcon from "@mui/icons-material/Edit";
@@ -16,43 +16,49 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import CircularProgress from "@mui/material/CircularProgress";
-import './pages_style.css'
-
+import "./pages_style.css";
 
 export class Admin extends Component {
   constructor(props) {
     super(props);
     this.state = {
       fetchData: [],
-      page: 0,
+      skip: 0,
       rowsPerPage: 15,
+      numOfData: null,
       shouldDeletedId: null,
       openModal: false,
-      progress:false,
+      progress: false,
     };
   }
 
   componentDidMount() {
     if (this.state.fetchData.length === 0) {
-      fetch("https://janta-mabi-quiz.onrender.com/").then((response) => {
+      fetch(`https://janta-mabi-quiz.onrender.com/admin_question/${this.state.skip}`).then((response) => {
         response.json().then((result) => {
-          this.setState({ fetchData: result });
+          console.log(result)
+          this.setState({
+            fetchData: result.data,
+            numOfData: result.numOfItems,
+          });
+        });
+      });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.skip !== prevState.skip) {
+      fetch(`https://janta-mabi-quiz.onrender.com/admin_question/${this.state.skip}`).then((response) => {
+        response.json().then((result) => {
+          this.setState({
+            fetchData: result.data,
+            numOfData: result.numOfItems,
+          });
         });
       });
     }
   }
 
-  handleChangePage = (event, newPage) => {
-    this.setState({ page: newPage });
-  };
-
-  handleChangeRowsPerPage = (event) => {
-    this.setState({ rowsPerPage: +event.target.value });
-    // setRowsPerPage(+event.target.value);
-    this.setState({ page: 0 });
-    // setPage(0);
-  };
-
+ 
   ModalStyle = {
     position: "absolute",
     top: "50%",
@@ -62,34 +68,33 @@ export class Admin extends Component {
     bgcolor: "background.paper",
     boxShadow: 24,
     p: 4,
-    borderRadius:"8px",
-    border:"2px solid green"
+    borderRadius: "8px",
+    border: "2px solid green",
   };
 
   font_size = {
-    fontSize:"1.06rem",
-    color:"#292f33",
-    fontFamily: 'Roboto',
-
-  }
-
+    fontSize: "1.06rem",
+    color: "#292f33",
+    fontFamily: "Roboto",
+  };
 
   handleDelete = () => {
-
-    this.setState({progress:true})
-    fetch(`https://janta-mabi-quiz.onrender.com/delete/${this.state.shouldDeletedId}`, {
-      method: "delete",
-    }).then((response)=>{
-      const newData = this.state.fetchData.filter((data) => this.state.shouldDeletedId !== data._id);
-      this.setState({ fetchData: newData,openModal:false,progress:false });
-
-    })
-   
-
-   
+    this.setState({ progress: true });
+    fetch(
+      `https://janta-mabi-quiz.onrender.com/delete/${this.state.shouldDeletedId}`,
+      {
+        method: "delete",
+      }
+    ).then((response) => {
+      const newData = this.state.fetchData.filter(
+        (data) => this.state.shouldDeletedId !== data._id
+      );
+      this.setState({ fetchData: newData, openModal: false, progress: false });
+    });
   };
 
   render() {
+    console.log(this.state.numOfData);
     return (
       <>
         <TableContainer component={Paper}>
@@ -106,47 +111,74 @@ export class Admin extends Component {
             aria-describedby="modal-modal-description"
           >
             <Box sx={this.ModalStyle}>
-              <Typography className="delete_modal" id="modal-modal-title" variant="h6" component="h2">
-              <span className="progress_circle_div"> Are you sure?
-                {this.state.progress && <CircularProgress sx={{marginTop:"10px"}}/>}
+              <Typography
+                className="delete_modal"
+                id="modal-modal-title"
+                variant="h6"
+                component="h2"
+              >
+                <span className="progress_circle_div">
+                  {" "}
+                  Are you sure?
+                  {this.state.progress && (
+                    <CircularProgress sx={{ marginTop: "10px" }} />
+                  )}
                 </span>
               </Typography>
 
               <div className="button_container">
-                <Button variant="outlined" disabled={this.state.progress}  onClick={()=>this.setState({openModal:false})}>cancel</Button>
+                <Button
+                  variant="outlined"
+                  disabled={this.state.progress}
+                  onClick={() => this.setState({ openModal: false })}
+                >
+                  cancel
+                </Button>
 
-                <Button variant="outlined" disabled={this.state.progress} onClick={this.handleDelete}>Delete</Button>
+                <Button
+                  variant="outlined"
+                  disabled={this.state.progress}
+                  onClick={this.handleDelete}
+                >
+                  Delete
+                </Button>
               </div>
             </Box>
           </Modal>
-
 
           {this.state.fetchData.length > 0 && (
             <Table aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell sx={this.font_size} align="left">Questions</TableCell>
-                  <TableCell sx={this.font_size} align="right">Option1</TableCell>
-                  <TableCell sx={this.font_size} align="right">Option2</TableCell>
-                  <TableCell sx={this.font_size} align="right">Option3</TableCell>
-                  <TableCell sx={this.font_size} align="right">Answer</TableCell>
-                  <TableCell sx={this.font_size} align="center">tools</TableCell>
+                  <TableCell sx={this.font_size} align="left">
+                    Questions
+                  </TableCell>
+                  <TableCell sx={this.font_size} align="right">
+                    Option1
+                  </TableCell>
+                  <TableCell sx={this.font_size} align="right">
+                    Option2
+                  </TableCell>
+                  <TableCell sx={this.font_size} align="right">
+                    Option3
+                  </TableCell>
+                  <TableCell sx={this.font_size} align="right">
+                    Answer
+                  </TableCell>
+                  <TableCell sx={this.font_size} align="center">
+                    tools
+                  </TableCell>
                 </TableRow>
               </TableHead>
 
               <TableBody>
                 {this.state.fetchData
-                  .slice(
-                    this.state.page * this.state.rowsPerPage,
-                    this.state.page * this.state.rowsPerPage +
-                      this.state.rowsPerPage
-                  )
-                  .map((row, index) => (
+                .map((row, index) => (
                     <TableRow key={row._id}>
                       <TableCell
                         component="th"
                         sx={this.font_size}
-                        style={{  fontWeight: "500" }}
+                        style={{ fontWeight: "500" }}
                         scope="row"
                       >
                         {row.question}
@@ -161,22 +193,22 @@ export class Admin extends Component {
                         {row.option1}
                       </TableCell>
                       <TableCell
-                      sx={this.font_size}
+                        sx={this.font_size}
                         style={{ fontWeight: "500" }}
                         align="right"
                       >
                         {row.option2}
                       </TableCell>
                       <TableCell
-                      sx={this.font_size}
+                        sx={this.font_size}
                         tyle={{ fontWeight: "500" }}
                         align="right"
                       >
                         {row.option3}
                       </TableCell>
                       <TableCell
-                      sx={this.font_size}
-                        style={{  fontWeight: "500" }}
+                        sx={this.font_size}
+                        style={{ fontWeight: "500" }}
                         align="right"
                       >
                         {row.correctAnswer}
@@ -194,19 +226,25 @@ export class Admin extends Component {
                           }}
                         >
                           <Link to={`/update/${row._id}`}>
-                            <EditIcon sx={{height:"1.5rem"}}
+                            <EditIcon
+                              sx={{ height: "1.5rem" }}
                               style={{ padding: "2px", margin: "0px 5px" }}
                             />
                           </Link>
                           {/* for update */}
                           <DeleteIcon
-                          sx={{height:"1.5rem"}}
+                            sx={{ height: "1.5rem" }}
                             style={{
                               cursor: "pointer",
                               padding: "2px",
                               margin: "0px 5px",
                             }}
-                            onClick={() => this.setState({openModal:true, shouldDeletedId:row._id} )}
+                            onClick={() =>
+                              this.setState({
+                                openModal: true,
+                                shouldDeletedId: row._id,
+                              })
+                            }
                           />
                         </div>
                       </TableCell>
@@ -215,17 +253,20 @@ export class Admin extends Component {
               </TableBody>
             </Table>
           )}
+          <Pagination
+            style={{ marginBottom: "10px", padding: "8px 6px", position:"absolute",right:0, marginRight:"20px" }}
+            count={Math.floor(this.state.numOfData / 10) - 1}
+            onChange={(e, value) => {
+              this.setState({ skip: value * 10 });
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }}
+            variant="outlined"
+            shape="rounded"
+          />
         </TableContainer>
-        <TablePagination
-        sx={{minHeight:"5vh", fontSize:"1.2rem"}}
-          rowsPerPageOptions={[15, 35, 100]}
-          count={this.state.fetchData.length}
-          rowsPerPage={this.state.rowsPerPage}
-          page={this.state.page}
-          component="div"
-          onPageChange={this.handleChangePage}
-          onRowsPerPageChange={this.handleChangeRowsPerPage}
-        />
       </>
     );
   }
